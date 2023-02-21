e78e5b61cd7a3dff56256ba48b5fafbf7cf2a0aa
Andrii Zhokh <andriizhokh@gmail.com>
2023-02-21 09:28:37 +0200

    updated emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -90,7 +90,7 @@
 
 (use-package doom-themes
   :init
-  (load-theme 'doom-tokyo-night t)
+  (load-theme 'doom-nord-aurora t)
   :config
   (setq doom-themes-enable-bold t
 	doom-themes-enable-italic t))
@@ -181,6 +181,20 @@
   ("k" text-scale-decrease "out")
   ("f" nil "finished" :exit t))
 
+(use-package projectile
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :custom ((projectile-completion-system 'ivy))
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :init
+  (when (file-directory-p "~/Projects/Code")
+    (setq projectile-project-search-path '("~/Projects/Code")))
+  (setq projectile-switch-project-action #'projectile-dired))
+
+(use-package counsel-projectile
+  :config (counsel-projectile-mode))
+
 					; Keybindings
 ;; Global
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit) ; escape to quit prompts
@@ -212,7 +226,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(hydra evil-collection general all-the-icons doom-themes helpful ivy-rich which-key rainbow-delimiters command-log-mode doom-modeline use-package markdown-mode evil counsel)))
+   '(counsel-projectile projectile hydra evil-collection general all-the-icons doom-themes helpful ivy-rich which-key rainbow-delimiters command-log-mode doom-modeline use-package markdown-mode evil counsel)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
