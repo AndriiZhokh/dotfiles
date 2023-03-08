6c863daaeba8c0f8214633e60e97e75124b09724
Andrii Zhokh <andriizhokh@gmail.com>
2023-03-08 18:04:36 +0200

    updated emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,18 +1,25 @@
-(scroll-bar-mode -1)                                                ; hide scroll bar
-(tool-bar-mode -1)                                                  ; hide tool bar
-(tooltip-mode -1)                                                   ; disable tooltips
-(electric-pair-mode 1)                                              ; auto close brackets
-(menu-bar-mode -1)                                                  ; hide menu bar
-(global-hl-line-mode 1)                                             ; highlight current line
-(column-number-mode)                                                ; display column number in modeline
-(global-display-line-numbers-mode 1)                                ; display line numbers
-(set-fringe-mode 10)                                                ; vertical margins
+(scroll-bar-mode -1)                                       ; hide scroll bar
+(tool-bar-mode -1)                                         ; hide tool bar
+(tooltip-mode -1)                                          ; disable tooltips
+(electric-pair-mode 1)                                     ; auto close brackets
+(menu-bar-mode -1)                                         ; hide menu bar
+(global-hl-line-mode 1)                                    ; highlight current line
+(column-number-mode)                                       ; display column number in modeline
+(global-display-line-numbers-mode 1)                       ; display line numbers
+(set-fringe-mode 10)                                       ; vertical margins
 
-(setq display-line-numbers-type 'relative)                          ; display relative line numbers
-(setq inhibit-startup-message t)                                    ; do not show default startup screen
-(setq visible-bell t)                                               ; set up visible bell
+(setq display-line-numbers-type 'relative)                 ; display relative line numbers
+(setq inhibit-startup-message t)                           ; do not show default startup screen
+(setq visible-bell t)                                      ; set up visible bell
 (setq backup-directory-alist
-      `(("." . ,(concat user-emacs-directory "backups"))))          ; set back up directory to backup files
+      `(("." . ,(concat user-emacs-directory "backups")))) ; set back up directory to backup files
+
+(put 'dired-find-alternate-file 'disabled nil)
+
+(dolist (mode '(org-mode-hook
+                term-mode-hook
+                shell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
 (set-face-attribute 'default nil :font "Cascadia Code" :height 110) ; set up font
 
@@ -22,18 +29,11 @@
 ;;Set the variable pitch face
 (set-face-attribute 'variable-pitch nil :font "Arial" :height 130)
 
-;; Disable line numbers for some modes
-(dolist (mode '(org-mode-hook
-		term-mode-hook
-		shell-mode-hook))
-  (add-hook mode (lambda () (display-line-numbers-mode 0))))
-
-;; Initialize package sources
 (require 'package)
 
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
-			 ("org" . "https://orgmode.org/elpa/")
-			 ("elpa" . "https://elpa.gnu.org/packages/")))
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
 
 (package-initialize)
 (unless package-archive-contents
@@ -47,40 +47,35 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-;; global-command-log-mode need to be enabled first
-;; clm/toggle-command-log-buffer C-c o to display buffer that will show your current pressed keys and commands
 (use-package command-log-mode)
 
 (use-package ivy
   :diminish
   :bind (("C-s" . swiper)
-	 :map ivy-minibuffer-map
-	 ("TAB" . ivy-alt-done)
-	 ("C-l" . ivy-alt-done)
-	 ("C-j" . ivy-next-line)
-	 ("C-k" . ivy-previous-line)
-	 :map ivy-switch-buffer-map
-	 ("C-k" . ivy-previous-line)
-	 ("C-l" . ivy-done)
-	 ("C-d" . ivy-switch-buffer-kill)
-	 :map ivy-reverse-i-search-map
-	 ("C-k" . ivy-previous-line)
-	 ("C-d" . ivy-reverse-i-search-kill))
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
   :config
   (ivy-mode 1))
 
 (use-package counsel
   :bind (("M-x" . counsel-M-x)
-	 ("C-x b" . counsel-ibuffer)
-	 ("C-x C-f" . counsel-find-file)
-	 :map minibuffer-local-map
-	 ("C-r" . 'counsel-minibuffer-history))
+         ("C-x b" . counsel-ibuffer)
+         ("C-x C-f" . counsel-find-file)
+         :map minibuffer-local-map
+         ("C-r" . 'counsel-minibuffer-history))
   :config
   (setq ivy-initialinputs-alist nil))
 
-;; After first install run the
-;; M-x all-the-icons-install-fonts
-;; and select directory where do you want to install the font icons
 (use-package all-the-icons
   :if (display-graphic-p))
 
@@ -93,7 +88,7 @@
   (load-theme 'doom-solarized-light t)
   :config
   (setq doom-themes-enable-bold t
-	doom-themes-enable-italic t))
+        doom-themes-enable-italic t))
 
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
@@ -143,13 +138,13 @@
 ;; for some reason disables evil-mode on start
 (defun azh/evil-hook ()
   (dolist (mode '(custom-mode
-		  eshell-mode
-		  erc-mode
-		  circe-server-mode
-		  circe-chat-mode
-		  circe-query-mode
-		  sauron-mode
-		  term-mode))
+                  eshell-mode
+                  erc-mode
+                  circe-server-mode
+                  circe-chat-mode
+                  circe-query-mode
+                  sauron-mode
+                  term-mode))
     (add-to-list 'evil-emacs-state-modes mode)))
 
 (use-package evil
@@ -199,125 +194,123 @@
   :hook (org-mode . azh/org-mode-setup)
   :config
   (setq org-ellipsis " ㄱ"
-	org-hide-emphasis-markers t)
+        org-hide-emphasis-markers t)
 
   (setq org-agenda-start-with-log-mode t)
   (setq org-log-done 'time)
   (setq org-log-into-drawer t)
 
   (setq org-agenda-files
-	'("~/Notes/tasks.org"
-	  "~/Notes/birthdays.org"
-	  "~/Notes/habits.org"))
+        '("~/Notes/tasks.org"
+          "~/Notes/birthdays.org"
+          "~/Notes/habits.org"))
 
   (require 'org-habit)
   (add-to-list 'org-modules 'org-habit)
   (setq org-habit-graph-column 60)
 
   (setq org-todo-keywords
-	'((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d!)")
-	  (sequence "BACKLOG(b)" "PLAN(p)" "READY(r)" "ACTIVE(a)" "REVIEW(v)" "WAIT(w@/!)" "HOLD(h)" "|" "COMPLETED(c)" "CANC(k@)")))
+        '((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d!)")
+          (sequence "BACKLOG(b)" "PLAN(p)" "READY(r)" "ACTIVE(a)" "REVIEW(v)" "WAIT(w@/!)" "HOLD(h)" "|" "COMPLETED(c)" "CANC(k@)")))
 
   (setq org-refile-targets
-	'(("archive.org" :maxlevel . 1)
-	  ("tasks.org" :maxlevel . 1)))
+        '(("archive.org" :maxlevel . 1)
+          ("tasks.org" :maxlevel . 1)))
 
   ;; Save Org buffer after refiling!
   (advice-add 'org-refile :after 'org-save-all-org-buffers)
 
   ;; This doesn't work for some reason
   (setq org-tag-alist
-	'((:startgroup)
-	  ; Put mutually exclusive tags here
-	  (:endgroup)
-	  ("@errand" . ?E)
-	  ("@home" . ?H)
-	  ("@work" . ?W)
-	  ("agenda" . ?a)
-	  ("planning" . ?p)
-	  ("publish" . ?P)
-	  ("batch" . ?b)
-	  ("note" . ?n)
-	  ("idea" . ?i)))
+        '((:startgroup)
+          ; Put mutually exclusive tags here
+          (:endgroup)
+          ("@errand" . ?E)
+          ("@home" . ?H)
+          ("@work" . ?W)
+          ("agenda" . ?a)
+          ("planning" . ?p)
+          ("publish" . ?P)
+          ("batch" . ?b)
+          ("note" . ?n)
+          ("idea" . ?i)))
 
   ;; Configure custom agenda views
   (setq org-agenda-custom-commands
-	'(("d" "Dashboard"
-	   ((agenda "" ((org-deadline-warning-days 7)))
-	    (todo "NEXT"
-		  ((org-agenda-overriding-header "Next Tasks")))
-	    (tags-todo "agenda/ACTIVE" ((org-agenda-overriding-header "Active Projects")))))
+        '(("d" "Dashboard"
+           ((agenda "" ((org-deadline-warning-days 7)))
+            (todo "NEXT"
+                  ((org-agenda-overriding-header "Next Tasks")))
+            (tags-todo "agenda/ACTIVE" ((org-agenda-overriding-header "Active Projects")))))
 
-	  ("n" "Next Tasks"
-	   ((todo "NEXT"
-		  ((org-agenda-overriding-header "Next Tasks")))))
+          ("n" "Next Tasks"
+           ((todo "NEXT"
+                  ((org-agenda-overriding-header "Next Tasks")))))
 
-	  ;; Include tags with '+' exclude tags with '-'
-	  ("W" "Work Tasks" tags-todo "+work-email")
+          ;; Include tags with '+' exclude tags with '-'
+          ("W" "Work Tasks" tags-todo "+work-email")
 
-	  ;;Low-effort next actions
-	  ("e" tags-todo "+TODO=\"NEXT\"+Effort<15&+Effort>0"
-	   ((org-agenda-overriding-header "Low Effort Tasks")
-	    (org-agenda-max-todos 20)
-	    (org-agenda-files org-agenda-files)))
+          ;;Low-effort next actions
+          ("e" tags-todo "+TODO=\"NEXT\"+Effort<15&+Effort>0"
+           ((org-agenda-overriding-header "Low Effort Tasks")
+            (org-agenda-max-todos 20)
+            (org-agenda-files org-agenda-files)))
 
-	  ("w" "Workflow Status"
-	   ((todo "WAIT"
-		  ((org-agenda-overriding-header "Waiting on External")
-		   (org-agenda-files org-agenda-files)))
-	    (todo "RVIEW"
-		  ((org-agenda-overriding-header "In Review")
-		   (org-agenda-files org-agenda-files)))
-	    (todo "PLAN"
-		  ((org-agenda-overriding-header "In Planning")
-		   (org-agenda-todo-list-sublevels nil)
-		   (org-agenda-files org-agenda-files)))
-	    (todo "BACKLOG"
-		  ((org-agenda-overriding-header "Project Backlog")
-		   (org-agenda-todo-list-sublevels nil)
-		   (org-agenda-files org-agenda-files)))
-	    (todo "READY"
-		  ((org-agenda-overriding-header "Ready for Work")
-		   (org-agenda-files org-agenda-files)))
-	    (todo "ACTIVE"
-		  ((org-agenda-overriding-header "Active Projects")
-		   (org-agenda-files org-agenda-files)))
-	    (todo "COMPLETED"
-		  ((org-agenda-overriding-header "Completed Projects")
-		   (org-agenda-files org-agenda-files)))
-	    (todo "CANC"
-		  ((org-agenda-overriding-header "Canceled Projects")
-		   (org-agenda-files org-agenda-files)))))))
+          ("w" "Workflow Status"
+           ((todo "WAIT"
+                  ((org-agenda-overriding-header "Waiting on External")
+                   (org-agenda-files org-agenda-files)))
+            (todo "RVIEW"
+                  ((org-agenda-overriding-header "In Review")
+                   (org-agenda-files org-agenda-files)))
+            (todo "PLAN"
+                  ((org-agenda-overriding-header "In Planning")
+                   (org-agenda-todo-list-sublevels nil)
+                   (org-agenda-files org-agenda-files)))
+            (todo "BACKLOG"
+                  ((org-agenda-overriding-header "Project Backlog")
+                   (org-agenda-todo-list-sublevels nil)
+                   (org-agenda-files org-agenda-files)))
+            (todo "READY"
+                  ((org-agenda-overriding-header "Ready for Work")
+                   (org-agenda-files org-agenda-files)))
+            (todo "ACTIVE"
+                  ((org-agenda-overriding-header "Active Projects")
+                   (org-agenda-files org-agenda-files)))
+            (todo "COMPLETED"
+                  ((org-agenda-overriding-header "Completed Projects")
+                   (org-agenda-files org-agenda-files)))
+            (todo "CANC"
+                  ((org-agenda-overriding-header "Canceled Projects")
+                   (org-agenda-files org-agenda-files)))))))
 
   (setq org-capture-templates
-	`(("t" "Tasks / Projects")
-	  ("tt" "Task" entry (file+olp "~/Notes/tasks.org" "Inbox")
-	   "* TODO %?\n %U\n %a\n %i" :empty-lines 1)
+        `(("t" "Tasks / Projects")
+          ("tt" "Task" entry (file+olp "~/Notes/tasks.org" "Inbox")
+           "* TODO %?\n %U\n %a\n %i" :empty-lines 1)
 
-	  ("j" "Journal Entries")
-	  ("jj" "Journal" entry
-	   (file+olp+datetree "~/Notes/journal.org")
-	   "\n* %<%I:%M %p> - Journal :journal:\n\n%?\n\n"
-	   :clock-in :clock-resume
-	   :empty-lines 1)
-	  ("jm" "Meeting" entry
-	   (file+olp+datetree "~/Notes/journal.org")
-	   "* %<%I:%M %p> - %a :meetings:\n\n%?\n\n"
-	   :clock-in :clock-resume
-	   :empty-lines 1)
+          ("j" "Journal Entries")
+          ("jj" "Journal" entry
+           (file+olp+datetree "~/Notes/journal.org")
+           "\n* %<%I:%M %p> - Journal :journal:\n\n%?\n\n"
+           :clock-in :clock-resume
+           :empty-lines 1)
+          ("jm" "Meeting" entry
+           (file+olp+datetree "~/Notes/journal.org")
+           "* %<%I:%M %p> - %a :meetings:\n\n%?\n\n"
+           :clock-in :clock-resume
+           :empty-lines 1)
 
-	  ("w" "Workflows")
-	  ("we" "Checking Email" entry (file+olp+datetree "~/Notes/journal.org")
-	   "* Checking Email :email:\n\n%?" :clock-in :clock-resume :empty-lines 1)
+          ("w" "Workflows")
+          ("we" "Checking Email" entry (file+olp+datetree "~/Notes/journal.org")
+           "* Checking Email :email:\n\n%?" :clock-in :clock-resume :empty-lines 1)
 
-	  ("m" "Metrics Capture")
-	  ("mw" "Weight" table-line (file+headline "~/Notes/metrics.org" "Weight")
-	   "| %U | %^{Weight} | %^{Notes} |" :kill-buffer t)))
+          ("m" "Metrics Capture")
+          ("mw" "Weight" table-line (file+headline "~/Notes/metrics.org" "Weight")
+           "| %U | %^{Weight} | %^{Notes} |" :kill-buffer t)))
 
   (define-key global-map (kbd "C-c j")
     (lambda () (interactive) (org-capture nil "jj"))))
-
-
 
 (use-package org-bullets
   :after org
@@ -326,62 +319,63 @@
   (org-bullets-bullet-list '("Ⅰ" "Ⅱ" "Ⅲ" "Ⅳ" "Ⅴ" "Ⅵ" "Ⅶ" "Ⅷ" "Ⅸ" "Ⅹ" "Ⅺ" "Ⅻ")))
 
 (dolist (face '((org-level-1 . 1.2)
-		(org-level-2 . 1.1)
-		(org-level-3 . 1.05)
-		(org-level-4 . 1.0)
-		(org-level-5 . 1.1)
-		(org-level-6 . 1.1)
-		(org-level-7 . 1.1)
-		(org-level-8 . 1.1))))
+                (org-level-2 . 1.1)
+                (org-level-3 . 1.05)
+                (org-level-4 . 1.0)
+                (org-level-5 . 1.1)
+                (org-level-6 . 1.1)
+                (org-level-7 . 1.1)
+                (org-level-8 . 1.1))))
 
 (defun azh/org-mode-visual-fill ()
   (setq visual-fill-column-width 100
-	visual-fill-column-center-text t)
+        visual-fill-column-center-text t)
   (visual-fill-column-mode 1))
 
 (use-package visual-fill-column
   :defer t
   :hook (org-mode . azh/org-mode-visual-fill))
 
-					; Keybindings
-;; Global
+(org-babel-do-load-languages
+  'org-babel-load-languages
+  '((emacs-lisp . t)
+    (python . t)))
+
+(setq org-confirm-babel-evaluate nil)
+
+(push '("conf-unix" . conf-unix) org-src-lang-modes)
+
+;; Automatically tangle our Emacs.org config file when we save it
+(defun azh/org-babel-tangle-config ()
+  (when (string-equal (buffer-file-name)
+                      (expand-file-name "~/Notes/emacs.org"))
+    ;; Dynamic scoping to the rescue
+    (let ((org-confirm-babel-evaluate nil))
+      (org-babel-tangle))))
+
+(add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'azh/org-babel-tangle-config)))
+
+;; This is needed as of Org 9.2
+(require 'org-tempo)
+
+(add-to-list 'org-structure-template-alist '("sh" . "src shell"))
+(add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
+(add-to-list 'org-structure-template-alist '("py" . "src python"))
+
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit) ; escape to quit prompts
 (global-set-key (kbd "C-M-j") 'counsel-switch-buffer)   ; switch to buffer
 
-;; Map specific mode
 (define-key emacs-lisp-mode-map (kbd "C-x M-t") 'counsel-load-theme) ; load custom theme
-
-					; Commands
-;; C-h f --- describe function
-;; C-h v --- describe variable
-;; C-x C-f - find file
-;; C-c o --- toggle command log buffer
-;; C-x C-e - execute (eval) current specific block (not entire buffer)
-;; when M-x is started type M-o on some function and you can see additional options that you can choose
-
-					; TODOs
-;; TODO: Start Emacs in home directory
-;; TODO: font ligatures support
-;; TODO: do not open new dired buffer every time when navigating through folders
-;; TODO: syntax check for English and Ukrainian languages
-;; TODO: Do not wrap lines
-;; TODO: transperancy
-;; TODO: switch between separated windows and close that separated windows
-;; TODO: write bash script that will synchronize current config with dotfile repo folder
-;; TODO: markdown mode
-;; TODO: yaml mode
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(visual-fill-column visual-fill visual-fill-mode org-bullets yaml-mode forge magit counsel-projectile projectile hydra evil-collection general all-the-icons doom-themes helpful ivy-rich which-key rainbow-delimiters command-log-mode doom-modeline use-package markdown-mode evil counsel)))
+   '(visual-fill-column org-bullets magit counsel-projectile projectile hydra evil-collection evil general helpful ivy-rich which-key rainbow-delimiters doom-themes doom-modeline all-the-icons counsel ivy command-log-mode use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-(put 'dired-find-alternate-file 'disabled nil)
