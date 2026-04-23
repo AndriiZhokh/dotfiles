cb81c647bee296e56c032c9566203e1814545c7a
Andrii Zhokh <andriizhokh@gmail.com>
2026-04-23 09:43:06 +0300

    [neovim] fzf lua look for all files including hidden or ignored

--- neovim/lua/plugins/fzf-lua.lua
+++ neovim/lua/plugins/fzf-lua.lua
@@ -8,6 +8,7 @@ return {
 
     which_key.add {
       { '<leader>sf', fzf_lua.files, desc = '[S]earch [F]iles' },
+      { '<leader>sa', function() fzf_lua.files({ fd_opts = '--no-ignore --color=never --type f --hidden --follow --exclude .git' }) end, desc = '[S]earch [A]ll files (incl. gitignored)' },
       { '<leader>sg', fzf_lua.grep, desc = '[S]earch with [G]rep'},
       { '<leader><leader>', fzf_lua.buffers, desc = '[ ] Find existing buffers' },
     }
