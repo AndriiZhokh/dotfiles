91427d99e361611ab26232af7ae99da08a2ded05
Andrii Zhokh <andriizhokh@gmail.com>
2026-04-29 21:14:03 +0300

    [neovim] changed color scheme

--- neovim/lua/colorscheme.lua
+++ neovim/lua/colorscheme.lua
@@ -7,7 +7,7 @@ local COLORSCHEMES = {
   compline = 'compline',
   vesper = 'vesper'
 }
-local colorscheme = COLORSCHEMES.vesper
+local colorscheme = COLORSCHEMES.everforest
 
 local status_ok, _ = pcall(vim.cmd, 'colorscheme ' .. colorscheme)
 
